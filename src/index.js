cde50bb6c218e960f27b186f246788cf8d32fd97
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2020-06-21 21:44:37 +0545

    Add date-fns and format date

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import Observer from 'observer';
+import Observer from "observer";
+import { formatDistanceToNow, isValid, parseISO } from "date-fns";
 
 const observer = new Observer();
+
+const input = document.getElementById("input");
+const result = document.getElementById("time");
+
+input.oninput = (e) => {
+  observer.set(e.target.value);
+};
+
+observer.subscribe((value) => {
+  const date = parseISO(value);
+  if (value && isValid(date)) {
+    result.innerHTML = formatDistanceToNow(date);
+  }
+});
