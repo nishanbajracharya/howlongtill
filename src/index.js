b444f47d26c04ae5bd8e00fe700b14f25d7105e2
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2020-06-21 22:03:18 +0545

    Add date picker

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import Observer from 'observer';
+import flatpickr from 'flatpickr';
 import { formatDistanceToNow, isValid, parseISO } from 'date-fns';
+
+import 'flatpickr/dist/themes/airbnb.css';
 
 const observer = new Observer();
 
 const input = document.getElementById('input');
 const result = document.getElementById('time');
+
+flatpickr(input, {
+  altInput: true,
+  altFormat: 'F j, Y',
+  dateFormat: 'Y-m-d',
+  minDate: new Date(),
+});
 
 input.oninput = (e) => {
   observer.set(e.target.value);
